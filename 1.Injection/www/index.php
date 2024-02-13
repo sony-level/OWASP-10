6fdaed5ca6c3c462689f63846134915f1cfe291a
sony-level <sonydilane@gmail.com>
2024-02-13 14:45:32 +0100

    ajout d'element

--- 1.Injection/www/index.php
+++ 1.Injection/www/index.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 if(!isset($_SESSION["user"]) || empty($_SESSION["user"])) {
@@ -10,7 +11,7 @@ if(!isset($_SESSION["user"]) || empty($_SESSION["user"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>1ject10nP6pCek.qVQ&LMG;XSQ$m7lWBukEu!ri1&XZJ=NlUcZ0ibZVZIE3?aFiEOQY8i&GT390GG4nCwZI;iMgW&aqv5gz!T$X-IS0469</title>
+    <title>1ject10n</title>
     <style>
     .flag{
         text-align:center;
@@ -21,7 +22,7 @@ if(!isset($_SESSION["user"]) || empty($_SESSION["user"])) {
 </style>
 </head>
 <body>
-    <?= '<p class="flag">Flag{1ject10n}<p>'; session_destroy();?>
+    <?= '<p class="flag">Flag{1ject10nP6pCek.qVQ&LMG;XSQ$m7lWBukEu!ri1&XZJ=NlUcZ0ibZVZIE3?aFiEOQY8i&GT390GG4nCwZI;iMgW&aqv5gz!T$X-IS0469}<p>'; session_destroy();?>
 </body>
 </html>
 
